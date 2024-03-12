9424f3f81da296642db3160aa336afc9cb99eec9
Helin200203 <151860826+Helin200203@users.noreply.github.com>
2024-03-12 13:52:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,50 +1,56 @@
 import json
-from my_classes import Person, Experiment
+from datetime import datetime
+from my_classes import Subject, Examiner, Experiment
 
 def main():
-    subject_or_supervisor = input("Sind Sie der Tester, der Patient oder handelt es sich hier um beides? (subject/supervisor/beides): ")
+    subject_or_supervisor = input("Sind Sie der subject, der Psupervisor oder handelt es sich hier um beides? (subject/supervisor/beides): ")
     if subject_or_supervisor.lower() == 'supervisor':
-        print("Bitte geben Sie die Daten für die Patienten ein:")
+        print("Bitte geben Sie die Daten für des Supervisors ein:")
         supervisor_first_name = input("Vorname: ")
         supervisor_last_name = input("Nachname: ")
         supervisor_sex = input("Geschlecht (male/female): ")
-        supervisor_age = int(input("Alter: "))
-        supervisor_max_hr_bpm = int(input("estimate_max_hr: "))
-        supervisor = build_person(supervisor_first_name, supervisor_last_name, supervisor_sex, supervisor_age, supervisor_max_hr_bpm )
+        birthdate = input("Geburtsdatum (YYYY-MM-DD): ")
+        field_of_study = input("Fachgebiet: ")
+        supervisor = Examiner(first_name, last_name, sex, birthdate, field_of_study)
+        
     elif subject_or_supervisor.lower() == 'subject':
         print("Bitte geben Sie den Namen des testers ein:")
         subject_first_name = input("Vorname: ")
         subject_last_name = input("Nachname: ")
         subject_sex = input("Geschlecht (male/female): ")
-        subject_age = int(input("Alter: "))
-        subject = build_person(subject_first_name, subject_last_name, subject_sex, subject_age)
+        birthdate = input("Geburtsdatum (YYYY-MM-DD): ")
+        subject = Subject(first_name, last_name, sex, birthdate)
+        
     elif subject_or_supervisor.lower() == 'beides':
-        print("Bitte geben sie zuerst die daten der Patienten ein und anschließend die des Testers:")
+        print("Bitte geben Sie zuerst die Daten des Supervisors ein:")
         supervisor_first_name = input("Vorname: ")
         supervisor_last_name = input("Nachname: ")
         supervisor_sex = input("Geschlecht (male/female): ")
-        supervisor_age = int(input("Alter: "))
-        supervisor_max_hr_bpm = input("estimate_max_hr")
-        supervisor = build_person(supervisor_first_name, supervisor_last_name, supervisor_sex, supervisor_age, supervisor_max_hr_bpm )
+        birthdate = input("Geburtsdatum (YYYY-MM-DD): ")
+        field_of_study = input("Fachgebiet: ")
+        supervisor = Examiner(first_name, last_name, sex, birthdate, field_of_study)
+
         
+        print("Bitte geben Sie jetzt die Daten des Subjects ein:")
         subject_first_name = input("Vorname: ")
         subject_last_name = input("Nachname: ")
         subject_sex = input("Geschlecht (male/female): ")
-        subject_age = int(input("Alter: "))
-        subject = build_person(subject_first_name, subject_last_name, subject_sex, subject_age)
+        birthdate = input("Geburtsdatum (YYYY-MM-DD): ")
+        subject = Subject(first_name, last_name, sex, birthdate)
     else:
         print("Falsche Eingabe")
         
     
-    experiment_name = input("Name des Experiments: ")
-    experiment_date = input("Datum des Experiments: ")
-    experiment = build_experiment(experiment_name, experiment_date, supervisor, subject)
+    eexperiment_name = input("Name des Experiments: ")
+    experiment_date = input("Datum des Experiments (YYYY-MM-DD): ")
+    experiment_date = datetime.strptime(experiment_date, "%Y-%m-%d")
+    experiment = Experiment(experiment_name, experiment_date, supervisor, subject)
 
     print("Experiment-Daten:")
-    print(experiment)
+    print(vars(experiment))
   
-    with open("experiment.json", "w") as outfile:
-        json.dump(experiment.__dict__, outfile, default=lambda o: o.__dict__, indent=4)
+   with open("experiment.json", "w") as outfile:
+        json.dump(vars(experiment), outfile, default=lambda o: vars(o), indent=4)
 
     print("Die Experiment-Daten wurden in 'experiment.json' gespeichert.")
 
