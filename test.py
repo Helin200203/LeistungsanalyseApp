00a9f7d391ecb630e377bfb1400bf32e6ebe073a
Helin200203 <151860826+Helin200203@users.noreply.github.com>
2024-03-12 13:23:47 +0100

    Create test.py
    Wenn Sie nicht für jeden Testfall die Daten von Hand eingeben wollen, können eine Kopie der main.py namens test.py anlegen und dort die Testfälle ohne input() implementieren

---
+++ test.py
@@ -0,0 +1,17 @@
+import json
+from my_classes import Person, Experiment
+
+def test():
+  
+    supervisor = build_person("Helin", "Hussein", "female", 21, "estimate_max_hr")
+    subject_name = build_person("Helium", "stickstoff", "male", 41)
+    
+    experiment = build_experiment("Belastungsekg", "12.03.2024", supervisor, subject)
+
+    with open("experiment_test.json", "w") as outfile:
+        json.dump(experiment.__dict__, outfile, default=lambda o: o.__dict__, indent=4)
+
+    print("Test ist abgeschlossen. Daten wurden in 'experiment_test.json' gespeichert.")
+
+if __name__ == "__main__":
+    test()
