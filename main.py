b196ab35a27fe840a55c76f4ec59bbb0db491662
Jonathan Hermann <163886874+jonny1108@users.noreply.github.com>
2024-04-26 15:45:30 +0200

    Sakai-Aufgabe 6.2-fertig

--- main.py
+++ main.py
@@ -2,13 +2,8 @@ import json
 import my_classes as mc
 
 # Funktion zur Abfrage von Personendaten
-def ask_for_person_info(role):
-    print(f"Geben Sie die Informationen für {role} ein:")
-    first_name = input("Vorname: ")
-    last_name = input("Nachname: ")
-    sex = input("Geschlecht (male/female): ")
-    age = int(input("Alter: "))
-    return mc.Person(first_name, last_name, sex, age)
+
+
 
 # Hauptfunktion
 def main():
@@ -16,19 +11,27 @@ def main():
     experiment_name = input("Name des Experiments: ")
     date = input("Datum (dd.mm.yyyy): ")
 
-    # Informationen zum Supervisor und Versuchsperson
-    supervisor = ask_for_person_info("Supervisor")
-    subject = ask_for_person_info("Versuchsperson")
+    print(f"Geben Sie die Informationen für Subjekt ein:")
+    first_name = input("Vorname: ")
+    last_name = input("Nachname: ")
+    sex = input("Geschlecht (male/female): ")
+    birthdate = input("Birthdate: ")
+    study_area = input("study area: ")
+
+    subject = mc.Subject(first_name, last_name, birthdate, study_area, sex)
+
+    print(f"Geben Sie die Informationen für Supervisor ein:")
+    first_name = input("Vorname: ")
+    last_name = input("Nachname: ")
+    Kürzel = input("Kürzel: ")
+
+    supervisor = mc.Supervisor(first_name, last_name, Kürzel)
 
     # Erstellen und Drucken des Experiment-Dictionary
     experiment = mc.Experiment(experiment_name, date, supervisor.__dict__, subject.__dict__)
     print("\nExperiment-Details:")
     print(experiment)
-
-    # Speichern der Daten in einer JSON-Datei
-    with open("experiment_details.json", "w") as outfile:
-        json.dump(experiment.__dict__, outfile)
+    mc.Experiment.save(experiment, "experiment.json")
 
 if __name__ == "__main__":
     main()
-
--- my_classes.py
+++ my_classes.py
@@ -4,20 +4,32 @@ from datetime import datetime
 import json
 
 class Person:
-    def __init__(self, first_name, last_name, sex, age):
+    def __init__(self, first_name, last_name):
         self.first_name = first_name
         self.last_name = last_name
-        self.sex = sex
-        self.age = age
-        __dict__ = {"first_name":self.first_name,"last_name": self.last_name,"sex":self.sex,"age":self.age}
 
     def save(self, filename):
         # Speichert die Attribute der Person als JSON-Datei
         with open(filename, 'w') as file:
             json.dump(self.__dict__, file)
 
+class Subject(Person):
+    def __init__(self, first_name, last_name, birthdate, study_area, sex):
+        super().__init__(first_name, last_name)
+        self.sex = sex
+        self.study_area = study_area
+        self._birthdate = birthdate  # Verstecktes Attribut für das Geburtsdatum
+
     def estimate_max_hr(self):
-        return 220 - self.age
+        # Berechnung der maximalen Herzfrequenz basierend auf dem Alter
+        today = datetime.now()
+        age = today.year - self._birthdate.year
+        return 220 - age
+    
+class Supervisor(Person):
+    def __init__(self, first_name, last_name, Kürzel):
+        super().__init__(first_name, last_name)
+        self.Kürzel = Kürzel
 
 class Experiment:
     def __init__(self,experiment_name, date, supervisor, subject):
